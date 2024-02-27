543e2e6b2c7305c3970f2b3feebdd52b156f76f5
mariobou <7655@holbertonstudents.com>
2024-02-27 02:05:14 +0000

    1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Define a class Square."""
+
+
+class Square:
+    """Initialize a new Square.
+    Args:
+        size (int): The size of the new square.
+    """
+    self._size = size
