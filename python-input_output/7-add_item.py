e75a4b96839c8b7ea103ea14d02db4797793f01e
mariobou <7655@holbertonstudents.com>
2024-03-05 01:25:30 +0000

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,7 +3,7 @@
 import sys
 
 if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
     load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
 
@@ -13,3 +13,4 @@ if __name__ == "__main__":
         items = []
     items.extend(sys.argv[1:])
     save_to_json_file(items, "add_item.json")
+
