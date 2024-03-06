9f4b9d112d89f058a2696702727abfdd3952a2ed
mariobou <7655@holbertonstudents.com>
2024-03-06 03:04:20 +0000

    coreccion base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -24,8 +24,8 @@ class Base:
     def to_json_string(list_dictionaries):
         """ convert result to Json
         """
-        if list_dictionaries is None or list_dictionaries == []:
-            return []
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
